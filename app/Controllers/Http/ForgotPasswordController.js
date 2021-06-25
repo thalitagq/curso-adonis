0be3d8afad884477cc1358141273bbc514c4a3a8
thalita <thalitagq@outlook.com>
2021-06-25 16:39:31 -0300

    feat: :sparkles: add cotent of classes from 8 to 12

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,8 +1,9 @@
 "use strict";
 
+const moment = require('moment')
 const crypto = require("crypto");
 const User = use("App/Models/User");
-const Mail = use('Mail')
+const Mail = use("Mail");
 
 class ForgotPasswordController {
   async store({ request, response }) {
@@ -16,19 +17,50 @@ class ForgotPasswordController {
       await user.save();
 
       await Mail.send(
-        ['emails.forgot_password'],
-        { email, toke: user.token, link: `${request.input('redirect_url')}?token=${user.token}`},
-        message=>{
+        ["emails.forgot_password"],
+        {
+          email,
+          token: user.token,
+          link: `${request.input("redirect_url")}?token=${user.token}`,
+        },
+        (message) => {
           message
             .to(user.email)
-            .from('thalitagq0@gmail.com', 'Thalita')
-            .subject('Password Recovery')
+            .from("thalitagq0@gmail.com", "Thalita")
+            .subject("Password Recovery");
         }
-      )
-
+      );
     } catch (error) {
       return response.status(error.status).send({
         error: { message: "Something went wrong. Does this email exist?" },
+      });
+    }
+  }
+
+  async update({ request, response }) {
+    try {
+      const {token, password} = request.all()
+      const user = await User.findByOrFail('token', token)
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+        if (tokenExpired) {
+          return response
+            .status(401)
+            .send({error: { message: 'Recovery token expired'}})
+        }
+
+        user.token = null
+        user.token_created_at = null
+        user.password = password
+
+        await user.save()
+    } catch (error) {
+      return response.status(error.status).send({
+        error: {
+          message: "Something went wrong when resetting your password.",
+        },
       });
     }
   }
