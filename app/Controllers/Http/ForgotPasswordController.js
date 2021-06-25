a5169c7694ee9c39f17aa327c8d5c6bbdfc5f8a1
thalita <thalitagq@outlook.com>
2021-06-25 14:43:04 -0300

    feat: :sparkles: add content from class 1  to class  7

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const crypto = require("crypto");
+const User = use("App/Models/User");
+const Mail = use('Mail')
+
+class ForgotPasswordController {
+  async store({ request, response }) {
+    try {
+      const email = request.input("email");
+      const user = await User.findByOrFail("email", email);
+
+      user.token = crypto.randomBytes(10).toString("hex");
+      user.token_created_at = new Date();
+
+      await user.save();
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, toke: user.token, link: `${request.input('redirect_url')}?token=${user.token}`},
+        message=>{
+          message
+            .to(user.email)
+            .from('thalitagq0@gmail.com', 'Thalita')
+            .subject('Password Recovery')
+        }
+      )
+
+    } catch (error) {
+      return response.status(error.status).send({
+        error: { message: "Something went wrong. Does this email exist?" },
+      });
+    }
+  }
+}
+
+module.exports = ForgotPasswordController;
