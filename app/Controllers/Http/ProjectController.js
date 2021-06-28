9b3d06ba233530f030278b0bc20224740699e7a2
thalita <thalitagq@outlook.com>
2021-06-28 13:46:03 -0300

    feat: :sparkles: add content from classes 13 to 20

--- app/Controllers/Http/ProjectController.js
+++ app/Controllers/Http/ProjectController.js
@@ -1,8 +1,10 @@
-'use strict'
+"use strict";
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Project = use("App/Models/Project");
 
 /**
  * Resourceful controller for interacting with projects
@@ -17,7 +19,14 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index({ request }) {
+    const { page } = request.get()
+
+    const projects = await Project.query()
+      .with("user")
+      .paginate(page);
+
+    return projects;
   }
 
   /**
@@ -29,8 +38,7 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async create ({ request, response, view }) {
-  }
+  async create({ request, response, view }) {}
 
   /**
    * Create/save a new project.
@@ -40,7 +48,12 @@ class ProjectController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response, auth }) {
+    const data = request.only(["title", "description"]);
+
+    const project = await Project.create({ ...data, user_id: auth.user.id });
+
+    return project;
   }
 
   /**
@@ -52,7 +65,13 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
+  async show({ params }) {
+    const project = await Project.findOrFail(params.id);
+
+    await project.load("user");
+    await project.load("tasks");
+
+    return project;
   }
 
   /**
@@ -64,8 +83,7 @@ class ProjectController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async edit ({ params, request, response, view }) {
-  }
+  async edit({ params, request, response, view }) {}
 
   /**
    * Update project details.
@@ -75,7 +93,14 @@ class ProjectController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params, request }) {
+    const project = await Project.findOrFail(params.id);
+    const data = request.only(["title", "description"]);
+
+    project.merge(data);
+
+    await project.save();
+    return project;
   }
 
   /**
@@ -86,8 +111,11 @@ class ProjectController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params }) {
+    const project = await Project.findOrFail(params.id);
+
+    await project.delete();
   }
 }
 
-module.exports = ProjectController
+module.exports = ProjectController;
